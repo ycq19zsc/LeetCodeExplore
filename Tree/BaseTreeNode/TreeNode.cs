729ea8e9999d8ff6673ec7fe2c727ce2a70e2905
zsc <ycq19zsc@gmail.com>
2018-10-06 13:27:34 +0800

    add Tree base node

---
+++ Tree/BaseTreeNode/TreeNode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaseTreeNode
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
