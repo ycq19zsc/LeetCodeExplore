4d77ccc0cef876d3ba3c91ecc7df2c54ef9d98e5
zsc <ycq19zsc@gmail.com>
2018-10-06 17:24:10 +0800

    add Tree-IsSymmetric solution

---
+++ Tree/IsSymmetric/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using BaseTreeNode;
+
+namespace IsSymmetric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static bool IsSymmetric(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            return IsSymmetricHelper(root.left,root.right);
+
+        }
+
+        static bool IsSymmetricHelper(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null)
+            {
+                return true;
+            }
+
+            if (left == null || right == null)
+            {
+                return false;
+            }
+
+            if (left.val != right.val)
+            {
+                return false;
+            }
+
+            return IsSymmetricHelper(left.right, right.left) && IsSymmetricHelper(left.left, right.right);
+        }
+    }
+}
